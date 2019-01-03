4f195e89d11e50863e0c23a3df55bfb53cc30fef
Miranda Limonczenko <mlimonczenko@pivotal.io>
2019-01-03 15:31:20 -0800

    Add redirect for oss/pivotal branches [#162583014]

--- redirects.rb
+++ redirects.rb
@@ -1,3 +1,4 @@
 r301 '/', '/wiki/index.html'
+r301 '/wiki/pipelines/versions-branches.html', '/wiki/external/versions-branches.html'
 
 r301 '/wiki-new/text/create-a-warning-box.html', '/wiki-new/style/html-classes.html'
