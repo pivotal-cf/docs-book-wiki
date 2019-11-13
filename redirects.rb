6253148f0b7d6d8c7e1a51c2f61d71b1d14f5cfd
Severine Tymon <s.tymon@gmail.com>
2019-11-13 11:29:35 -0800

    add redirect for renamed file

--- redirects.rb
+++ redirects.rb
@@ -1,4 +1,5 @@
 r301 '/', '/wiki/index.html'
 r301 '/wiki/pipelines/versions-branches.html', '/wiki/external/versions-branches.html'
+r301 '/wiki/knowledgebase/deprecating-docs.html', '/wiki/knowledgebase/end-of-general-support-docs.html'
 
 r301 '/wiki-new/text/create-a-warning-box.html', '/wiki-new/style/html-classes.html'
